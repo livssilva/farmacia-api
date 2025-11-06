aca66c8f767d9244eb46ec50c5efbc9e9249e248
Lívia Azevedo <liviaazevedostz@gmail.com>
2025-11-06 13:42:22 -0300

    Corrigindo código do banco de dados PostgreSQL

--- infra/sql/init.sql
+++ infra/sql/init.sql
@@ -1,16 +1,17 @@
 CREATE TABLE Clientes ( 
-    id_cliente INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    cpf VARCHAR (13) UNIQUE NOT NULL,   
+    id_cliente INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     nome VARCHAR (50) NOT NULL,
-    nascimento DATE NOT NULL,
-    telefone VARCHAR (20) NOT NULL,
+	cpf VARCHAR (13) UNIQUE NOT NULL,
+	telefone VARCHAR (20) NOT NULL,
+    data_nascimento DATE NOT NULL,
     email VARCHAR (70) NOT NULL
 );
+
 
 CREATE TABLE Medicamentos ( 
     id_medicamento INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nome VARCHAR (50) NOT NULL,     
-    principioAtivo VARCHAR (50) NOT NULL,
-    dataValidade DATE NOT NULL,
+    principio_ativo VARCHAR (50) NOT NULL,
+    data_validade DATE NOT NULL,
     preco DECIMAL (10,2) NOT NULL
 );
